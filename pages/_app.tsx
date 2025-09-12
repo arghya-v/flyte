ca161ce389816b5fb021c7ad85a35f3f8e27fba7
Arghya <arghyavyas775@gmail.com>
2025-09-12 12:21:42 -0400

    debug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,14 @@ import "@/styles/globals.css";
 import "leaflet/dist/leaflet.css";
 import { Inter } from "next/font/google";
 import type { AppProps } from "next/app";
-
+import { Analytics } from "@vercel/analytics/next";
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <main className={inter.className}>
+        <Analytics />
         <Component {...pageProps} />
       </main>
       <div id="root-portal"></div>
